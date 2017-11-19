7e43b23eaeec0bef75542cd3389609b858106962
srikanth dasari <srikanth.dasari@outlook.com>
2017-11-19 14:18:56 -0500

    created Shell

---
+++ PrismWorkout/Wpf.UI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf.UI
+{
+    public class Bootstrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
